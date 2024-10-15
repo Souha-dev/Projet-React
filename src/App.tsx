8c28f065802c217c47d219f9c08be22341989b00
premierdepot <souhaila.omri3@gmail.com>
2024-10-15 17:31:06 +0100

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Home from "./Home";
+import Boutique from "./Boutique/Boutique";
+import { ShoppingCartProvider } from "./Utils/CartContext";
+import Panier from "./components/Panier/Panier";
+import SousPanier from "./components/SousP/SousPanier";
+import { CartDataProvider } from "./Utils/CartDataContext";
+
+// Assurez-vous que le chemin est correct
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ShoppingCartProvider>
+      <CartDataProvider>
+        <Router>
+          <div className="App">
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/boutique" element={<Boutique />} />
+              <Route path="/panier" element={<Panier />} />
+              <Route path="/sous" element={<SousPanier />} />
+            </Routes>
+          </div>
+        </Router>
+      </CartDataProvider>
+    </ShoppingCartProvider>
   );
 }
 
